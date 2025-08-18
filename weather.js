8c0e8744dfec4cfabe7f905a1f0d4e21acbaf8c3
Kalkidan404 <kalrokal1621@gmail.com>
2025-08-18 12:49:20 +0300

    update forecast

--- weather.js
+++ weather.js
@@ -18,6 +18,7 @@ function weatherinfo(response) {
   iconurl.innerHTML = `<img src="${response.data.condition.icon_url}"/>`;
   let time = document.querySelector(".time");
   time.innerHTML = formatDate(date);
+  getapi(response.data.city);
 }
 function formatDate(date) {
   let minutes = date.getMinutes();
@@ -43,6 +44,42 @@ function weather(city) {
   let apiurl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&units=metric`;
   axios.get(apiurl).then(weatherinfo);
 }
+function formatday(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
+  return days[date.getDay()];
+}
+function getapi(city) {
+  let apikey = "de072a3do70f178f6a0bt43f94a33afc";
+
+  let apiurl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apikey}&units=metric`;
+  axios.get(apiurl).then(forcast);
+}
+
+function forcast(response) {
+  let forecastHtml = "";
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
+      <div class="weather-forcast">
+        <div class="forcast-day">${formatday(day.time)}</div>
+        <div class="forcast-icon"><img src="${day.condition.icon_url}" </div>
+        <div class="forcast-temps">
+        <div class="forecast-temp"><strong>${Math.round(
+          day.temperature.maximum
+        )}°</strong></div>
+        <div class="forecast-temp">${Math.round(day.temperature.minimum)}°</div>
+      </div>
+      </div>
+  `;
+    }
+  });
+
+  let forecastElement = document.getElementById("forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
 
 function searchcity(event) {
   event.preventDefault();
@@ -50,3 +87,4 @@ function searchcity(event) {
   weather(searchinput.value);
 }
 searchbtn.addEventListener("click", searchcity);
+weather("paris");
